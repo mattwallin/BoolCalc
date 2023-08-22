3e702d12b869b5bbf72215d56c8e62a9cb6d29d7
Matt Wallin <matt.wallin2@gmail.com>
2023-08-22 15:35:36 -0700

    calc returns true if string is equal to not false

--- src/calc.spec.ts
+++ src/calc.spec.ts
@@ -1,10 +1,13 @@
 import calc from "./calc";
 
 describe('calc', () => {
-    it('returns false if no string is entered', () => {
+    it('returns false if string is empty', () => {
         expect(calc("")).toBeFalsy();
     });
     it('returns true if the string is equal to true', () => {
         expect(calc("true")).toBeTruthy();
+    });
+    it('returns true if string is equal to not false', () => {
+        expect(calc("not false")).toBeTruthy();
     });
 });
--- src/calc.ts
+++ src/calc.ts
@@ -3,5 +3,8 @@ export default function calc(str: string): boolean {
     if(str === "true") {
         flag = true;
     }
+    if(str === "not false") {
+        flag = true;
+    }
     return flag;
 }

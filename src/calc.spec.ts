ccc13426939da7b535ba4941cb8ea83b14c3e60d
Matt Wallin <matt.wallin2@gmail.com>
2023-08-23 15:07:54 -0700

    Refactored calc and implemented "OR" to return true if orflag is true and prior result is true

--- src/calc.spec.ts
+++ src/calc.spec.ts
@@ -4,13 +4,19 @@ describe('calc', () => {
     it('returns false if string is empty', () => {
         expect(calc("")).toBeFalsy();
     });
-    it('returns true if the string is equal to true', () => {
+    it('returns true if the string includes true', () => {
         expect(calc("true")).toBeTruthy();
     });
-    it('returns true if string is equal to not false', () => {
+    it('returns false if string includes false', () => {
+        expect(calc("false")).toBeFalsy();
+    });
+    it('returns true if string includes not false', () => {
         expect(calc("not false")).toBeTruthy();
     });
     it('returns false if string includes not true', () => {
-        expect(calc("not true")).toBeFalsy();
+        expect(calc("true or not true")).toBeTruthy();
+    });
+    it('returns true if string is "true or false"', () => {
+        expect(calc("false or not true")).toBeFalsy();
     });
 });
--- src/calc.ts
+++ src/calc.ts
@@ -1,13 +1,41 @@
+//Implement OR keyword
+//Implement AND keyword
+
 export default function calc(str: string): boolean {
-    let flag = false;
-    if(str.includes( "true")) {
-        flag = true;
-    }
-    if(str.includes("not false")) {
-        flag = true;
-    }
-    if(str.includes("not true")) {
-        flag = false;
-    }
-    return flag;
+    let result = false;
+    let notFlag = false;
+    let orFlag = false;
+    let andFlag = false;
+    let strArr = str.toLowerCase().split(" ");
+    strArr.map((curr) => {
+        if(result === true && orFlag) {
+            return true;
+        }
+        if(notFlag) {
+            if(curr === "true") {
+                result = false;
+            }
+            else {
+                result = true;
+            }
+            notFlag = false;
+        }
+        else if(curr === "true") {
+            result = true;
+        }
+        switch(curr) {
+            case "not":
+                notFlag = true;
+                break;
+            case "and":
+                andFlag = true;
+                break;
+            case "or":
+                orFlag = true;
+                break;
+            default:
+                break; 
+        }
+    })
+    return result;
 }

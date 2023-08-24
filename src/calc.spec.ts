858dd6811dec701c925f28b104b7e38421ecf5e1
Matt Wallin <matt.wallin2@gmail.com>
2023-08-24 01:55:45 -0700

    Calc returns true when string is "true" and false when string is "false"

--- src/calc.spec.ts
+++ src/calc.spec.ts
@@ -10,25 +10,4 @@ describe('calc', () => {
     it('returns false if string is "false"', () => {
         expect(calc("false")).toBeFalsy();
     });
-    it('returns true if string is "not false"', () => {
-        expect(calc("not false")).toBeTruthy();
-    });
-    it('returns true if string is "true or not true"', () => {
-        expect(calc("true or not true")).toBeTruthy();
-    });
-    it('returns false if string is "false or not true"', () => {
-        expect(calc("false or not true")).toBeFalsy();
-    });
-    it('returns false if string is "true and false"', () => {
-        expect(calc("true and false")).toBeFalsy();
-    });
-    it('returns true if string is "true or true or true and false"', () => {
-        expect(calc("true or true or true and false")).toBeTruthy();
-    });
-    it('returns true if string is "true or false and not false"', () => {
-        expect(calc("true or false and not false")).toBeTruthy();
-    });
-    it('returns false if string is "(true or true or true) and false"', () => {
-        expect(calc("(true or true or true) and false")).toBeFalsy();
-    });
 });
--- src/calc.ts
+++ src/calc.ts
@@ -1,48 +1,5 @@
 //Implement AND keyword
 
 export default function calc(str: string): boolean {
-    let result: boolean | undefined = undefined;
-    let notFlag = false;
-    let orFlag = false;
-    let andFlag = false;
-    let strArr = str.toLowerCase().split(" ");
-    strArr.map((curr) => {
-        if(result === false && andFlag) {
-            return false;
-        }
-        else if(result && orFlag) {
-            return true;
-        }
-        else if(notFlag) {
-            if(curr === "true") {
-                result = false;
-            }
-            else {
-                result = true;
-            }
-            notFlag = false;
-        }
-        else {
-            switch(curr) {
-                case "true":
-                    result = true;
-                    break;
-                case "false":
-                    result = false;
-                    break;
-                case "not":
-                    notFlag = true;
-                    break;
-                case "and":
-                    andFlag = true;
-                    break;
-                case "or":
-                    orFlag = true;
-                    break;
-                default:
-                    break; 
-            }
-        }
-    })
-    return result!;
+    return eval(str);
 }

480bad7f512ba994be19cf5b617725c1b0f64868
Matt Wallin <matt.wallin2@gmail.com>
2023-08-24 13:27:16 -0700

    Replace OR keyword in the string to ||

--- src/calc.spec.ts
+++ src/calc.spec.ts
@@ -101,4 +101,82 @@ describe('calc', () => {
     it('returns false when string is "((false) and not true)"', () => {
         expect(calc("((false) and not true)")).toBeFalsy();
     });
+    it('returns false when string is "((true) or not false)"', () => {
+        expect(calc("((true) or not false)")).toBeTruthy();
+    });
+    it('returns true if the string "true or true"', () => {
+        expect(calc("true or true")).toBeTruthy();
+    });
+    it('returns false if string is "false or false"', () => {
+        expect(calc("false or false")).toBeFalsy();
+    });
+        it('returns true if the string "true or false"', () => {
+        expect(calc("true or false")).toBeTruthy();
+    });
+    it('returns true if string is "false or true"', () => {
+        expect(calc("false or true")).toBeTruthy();
+    });
+    it('returns true when string is "(true or true)"', () => {
+        expect(calc("(true or true)")).toBeTruthy();
+    });
+    it('returns false when string is "(false or false)"', () => {
+        expect(calc("(false or false)")).toBeFalsy();
+    });
+    it('returns true when string is "(true or false)"', () => {
+        expect(calc("(true or false)")).toBeTruthy();
+    });
+    it('returns false when string is "(false or false)"', () => {
+        expect(calc("(false or false)")).toBeFalsy();
+    });
+    it('returns true when string is "((true or true))"', () => {
+        expect(calc("((true or true))")).toBeTruthy();
+    });
+    it('returns false when string is "((false or false))"', () => {
+        expect(calc("((false or false))")).toBeFalsy();
+    });
+    it('returns true when string is "((true or false))"', () => {
+        expect(calc("((true or true))")).toBeTruthy();
+    });
+    it('returns true when string is "((false or true))"', () => {
+        expect(calc("((false or true))")).toBeTruthy();
+    });
+    it('returns true when string is "((true) or true)"', () => {
+        expect(calc("((true) or true)")).toBeTruthy();
+    });
+    it('returns false when string is "((false) or false)"', () => {
+        expect(calc("((false) or false)")).toBeFalsy();
+    });
+    it('returns true when string is "((true) or false)"', () => {
+        expect(calc("((true) or false)")).toBeTruthy();
+    });
+    it('returns true when string is "((false) or true)"', () => {
+        expect(calc("((false) or true)")).toBeTruthy();
+    });
+    it('returns false if the string "not true or not true"', () => {
+        expect(calc("not true or not true")).toBeFalsy();
+    });
+    it('returns true if string is "not false or not false"', () => {
+        expect(calc("not false or not false")).toBeTruthy();
+    });
+    it('returns true if the string "not true or not false"', () => {
+        expect(calc("not true or not false")).toBeTruthy();
+    });
+    it('returns true if string is "not false or true"', () => {
+        expect(calc("not false or true")).toBeTruthy();
+    });
+    it('returns true when string is "(not true) or true"', () => {
+        expect(calc("(not true) or true")).toBeTruthy();
+    });
+    it('returns true when string is "(not false) or not false"', () => {
+        expect(calc("(not false) or not false")).toBeTruthy();
+    });
+    it('returns true when string is "((not true) or not false)"', () => {
+        expect(calc("((not true) or not false)")).toBeTruthy();
+    });
+    it('returns false when string is "((false) or not true)"', () => {
+        expect(calc("((false) or not true)")).toBeFalsy();
+    });
+    it('returns true when string is "((false) or not true)"', () => {
+        expect(calc("((true) or not false)")).toBeTruthy();
+    });
 });
--- src/calc.ts
+++ src/calc.ts
@@ -2,17 +2,20 @@
 interface correctionInterface {
     "not": string;
     "and": string;
+    "or": string;
 }
 
 export default function calc(str: string): boolean {
     const correctionObj: correctionInterface  = {
         "not": "!",
         "and": "&&",
+        "or": "||",
     }
+
     str = str.toLowerCase();
 
-    Object.keys(correctionObj).forEach((key) => {
-        str = str.replace(/not|and/g, matched => correctionObj[matched as keyof correctionInterface]);
-    })
-    return eval(str)
+    Object.keys(correctionObj).forEach(() => {
+        str = str.replace(/not|and|or/g, matched => correctionObj[matched as keyof correctionInterface]);
+    });
+    return eval(str);
 }

2e9f4d6015844e2ae6dd937954e03f10d7ce7446
Matt Wallin <matt.wallin2@gmail.com>
2023-08-24 02:05:52 -0700

    Returns true and false when the string has parentheses inside

--- src/calc.spec.ts
+++ src/calc.spec.ts
@@ -10,4 +10,16 @@ describe('calc', () => {
     it('returns false if string is "false"', () => {
         expect(calc("false")).toBeFalsy();
     });
+    it('returns true when string is "(true)"', () => {
+        expect(calc("(true)")).toBeTruthy();
+    });
+    it('returns true when string is "(false)"', () => {
+        expect(calc("(false)")).toBeFalsy();
+    });
+    it('returns true when string is "((true))"', () => {
+        expect(calc("((true))")).toBeTruthy();
+    });
+    it('returns true when string is "((false))"', () => {
+        expect(calc("((false))")).toBeFalsy();
+    });
 });

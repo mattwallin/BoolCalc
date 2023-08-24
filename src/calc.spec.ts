5fae6e311fc8de38418276a315066011f84e71a3
Matt Wallin <matt.wallin2@gmail.com>
2023-08-23 17:17:02 -0700

    Updated test case console logs to include the string value

--- src/calc.spec.ts
+++ src/calc.spec.ts
@@ -4,22 +4,31 @@ describe('calc', () => {
     it('returns false if string is empty', () => {
         expect(calc("")).toBeFalsy();
     });
-    it('returns true if the string includes true', () => {
+    it('returns true if the string "true"', () => {
         expect(calc("true")).toBeTruthy();
     });
-    it('returns false if string includes false', () => {
+    it('returns false if string is "false"', () => {
         expect(calc("false")).toBeFalsy();
     });
-    it('returns true if string includes not false', () => {
+    it('returns true if string is "not false"', () => {
         expect(calc("not false")).toBeTruthy();
     });
-    it('returns false if string includes not true', () => {
+    it('returns true if string is "true or not true"', () => {
         expect(calc("true or not true")).toBeTruthy();
     });
-    it('returns true if string is "true or false"', () => {
+    it('returns false if string is "false or not true"', () => {
         expect(calc("false or not true")).toBeFalsy();
     });
-    it('returns true if string is "true and false"', () => {
+    it('returns false if string is "true and false"', () => {
         expect(calc("true and false")).toBeFalsy();
-    })
+    });
+    it('returns true if string is "true or true or true and false"', () => {
+        expect(calc("true or true or true and false")).toBeTruthy();
+    });
+    it('returns true if string is "true or false and not false"', () => {
+        expect(calc("true or false and not false")).toBeTruthy();
+    });
+    it('returns false if string is "(true or true or true) and false"', () => {
+        expect(calc("(true or true or true) and false")).toBeFalsy();
+    });
 });
